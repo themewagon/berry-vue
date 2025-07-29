ee04b41650c5168782784a570fc534a8924208de
KhanAnupamShafi <deep71.bd@gmail.com>
2025-07-29 11:36:26 +0600

    initialize project

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
+import vuetify from 'vite-plugin-vuetify';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ['v-list-recognize-title'].includes(tag)
+        }
+      }
+    }),
+    vuetify({
+      autoImport: true
+    })
+  ],
+  base: '/berry-vue/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {}
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1024 * 1024 // Set the limit to 1 MB
+  },
+  optimizeDeps: {
+    exclude: ['vuetify'],
+    entries: ['./src/**/*.vue']
+  }
+});
